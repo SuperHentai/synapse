1158588894a673ea9790662c4dd9a57e12ec7bbb
Kaguya <1136358656@qq.com>
2015-05-06 23:42:23 +0800

    加入商业化菜单

---
+++ products.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('header.php');
+include ('logo.php');
+//require_once("dbConn.php");
+
+?>
+<div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+        <div class="col-lg-4">
+            <img class="img-circle" src="./images/shadowsocks.png" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+            <h2>Shadowsocks翻墙</h2>
+            <p></p><h5>在大陆，由于<a href="http://zh.wikipedia.org/wiki/%E9%98%B2%E7%81%AB%E9%95%BF%E5%9F%8E">GFW</a>的封锁与干扰，很多的国外网站都无法正常的访问，除了Facebook、twitter、YouTube以外，还有大量的学术网站，而各种VPN正在被重点打击，越来越不稳定。本项目的目的是提供一个稳定、轻量的代理方案，自由访问互联网。同时，拥有低延时的日本线路，可以用于访问限制日本IP的网站，例如舰队Collection游戏。</h5><p></p>
+            <div align="center"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./shadowsocks.php" target="_blank" role="button">购买</a></p></div>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="img-circle" src="./images/zhuji.png" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+            <h2>PHP 虚拟主机</h2>
+            <p></p><h5>有很多用户想建立自己的个人网站、博客，但是苦于不熟悉架设流程，本项目的目的是提供一个廉价、稳定、简便的网站架设平台，并提供必要的技术支持。<br><br><br></h5><p></p>
+            <div align="center"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./webserver.php" target="_blank" role="button">购买</a></div>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="img-circle" src="./images/cloudstore.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+            <h2>免费文件存储与外链服务</h2>
+            <p><h5>本项目免费提供了中小文件存储以及音乐、视频等文件的外链服务，连接稳定，响应速度快。</h5></p>
+            <div align="center"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./store.php" target="_blank" role="button">使用</a></p></div>
+        </div><!-- /.col-lg-4 -->
+
+
+    </div>
+
+</div>
+<?php
+include('footer.php');
+?>
+</body>
+</html>
+
