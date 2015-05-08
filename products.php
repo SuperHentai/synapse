b3f7f969f8519b03414850e57ce735ff6162fd92
Kaguya <1136358656@qq.com>
2015-05-08 10:52:14 +0800

    加入商业化菜单

--- products.php
+++ products.php
@@ -16,7 +16,7 @@ include ('logo.php');
                 <h2>Shadowsocks翻墙</h2>
                 <p></p><h5>在大陆，由于<a href="http://zh.wikipedia.org/wiki/%E9%98%B2%E7%81%AB%E9%95%BF%E5%9F%8E">GFW</a>的封锁与干扰，很多的国外网站都无法正常的访问，除了Facebook、twitter、YouTube以外，还有大量的学术网站，而各种VPN正在被重点打击，越来越不稳定。本项目的目的是提供一个稳定、轻量的代理方案，自由访问互联网。同时，拥有低延时的日本线路，可以用于访问限制日本IP的网站，例如舰队Collection游戏。</h5><p></p>
             </div>
-            <div align="center" style="bottom: 2px;position: absolute"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./shadowsocks.php" target="_blank" role="button">购买</a></p></div>
+            <div align="center"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./shadowsocks.php" target="_blank" role="button">购买</a></p></div>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4" style="height: 400px;position: relative">
             <div style="height: 350px" align="center">
@@ -24,7 +24,7 @@ include ('logo.php');
                 <h2>PHP 虚拟主机</h2>
                 <p></p><h5>有很多用户想建立自己的个人网站、博客，但是苦于不熟悉架设流程，本项目的目的是提供一个廉价、稳定、简便的网站架设平台，并提供必要的技术支持。<br><br><br></h5><p></p>
             </div>
-           <div align="center" style="bottom: 2px;position: absolute"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./webserver.php" target="_blank" role="button">购买</a></div>
+           <div align="center"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./webserver.php" target="_blank" role="button">购买</a></div>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4" style="height: 400px;position: relative">
             <div style="height: 350px" align="center">
@@ -32,7 +32,7 @@ include ('logo.php');
                 <h2>免费文件存储与外链服务</h2>
                 <p><h5>本项目免费提供了中小文件存储以及音乐、视频等文件的外链服务，连接稳定，响应速度快。</h5></p>
             </div>
-            <div align="center" style="bottom: 2px;position: absolute"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./store.php" target="_blank" role="button">使用</a></p></div>
+            <div align="center"><p><a class="button button-glow button-border button-rounded button-primary button-large" href="./store.php" target="_blank" role="button">使用</a></p></div>
         </div><!-- /.col-lg-4 -->
 
 
